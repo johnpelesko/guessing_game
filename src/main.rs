adbb3b1f1bab908e3abc83d11d8caaaf01bb43bf
johnpelesko <jpelesko@udel.edu>
2019-11-12 14:07:31 -0500

    completed game

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,36 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-	println!("Input test.");
+	println!("Guess a number beteween 1 & 100.");
 
 	let secret_number = rand::thread_rng().gen_range(1, 101);
 
-	println!("The secret number is: {}", secret_number);
+	loop {
 
-    println!("Input a string.");
+    	println!("Input a number.");
 
-    let mut guess = String::new();
+    	let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("If input is not valid this shows for the crash.");
+    	io::stdin().read_line(&mut guess)
+        	.expect("If input is not valid this shows for the crash.");
 
-    println!("Input was: {}", guess);
+		let guess: u32 = match guess.trim().parse() {
+			Ok(num) => num,
+			Err(_) => {
+				println!("Please input a number.");
+				continue;
+			}
+		};
 
-	match guess.cmp(&secret_number){
-	    Ordering::Less => println!("Too low."),
-		Ordering::Greater => println!("Too high."),
-		Ordering::Equal => println!("Correct"),
+    	println!("Input was: {}", guess);
+
+		match guess.cmp(&secret_number) {
+	    	Ordering::Less => println!("Too low."),
+			Ordering::Greater => println!("Too high."),
+			Ordering::Equal => {
+				println!("You win.");
+				break;
+			}
+		}
 	}
 }
